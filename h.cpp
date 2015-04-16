aaf278e4a4b1c2a7b9be9f0ef9a9d4df07e3545a
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2015-04-16 17:13:47 -0500

    Create h.cpp

---
+++ h.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello clase de herramientas!!" << endl;
+    for (int i =0; i < 9999; i++){
+        if (i==9999-1)
+            cout << "Un saludo desde GitHub!!!"<< endl;
+
+    }
+    return 0;
+}
